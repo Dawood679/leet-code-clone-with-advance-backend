e1702aa64b0f6e0a86b485b041b8843b9f5748df
Dawood <dawoodalam057@gmail.com>
2026-05-29 16:05:02 +0500

    env fixes:

--- apps/worker/src/judge0.js
+++ apps/worker/src/judge0.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../../../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 const axios = require('axios');
 
 const JUDGE0_URL = process.env.JUDGE0_URL || 'http://localhost:2358';
